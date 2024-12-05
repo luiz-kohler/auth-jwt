cf05c75e58da290fccded9175cba4f3faedd58b1
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-12-05 16:48:51 -0300

    creating user service

---
+++ API/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Infra;
+
+namespace API.Services
+{
+    public interface IUserService
+    {
+        Task Create(UserRequest request);
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task Create(UserRequest request)
+        {
+            var userWithSameEmail = await _userRepository.FirstOrDefaultWithWithNoTrackingAsync(user => user.Email == request.Email);
+
+            if (userWithSameEmail != null)
+                throw new BadHttpRequestException("there is already user with same email");
+
+            var user = new User
+            {
+                Email = request.Email,
+                Name = request.Name,
+                PasswordHashed = request.Password
+            };
+
+            await _userRepository.AddAsync(user);
+            await _userRepository.SaveChangesAsync();
+        }
+    }
+}
