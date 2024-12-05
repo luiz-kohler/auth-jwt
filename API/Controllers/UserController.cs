310fa5d993e6ad6c5d7955d7f3ed500c93d49234
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-12-05 16:55:47 -0300

    creating user controller

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using API.DTOs;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] UserRequest request)
+        {
+            _userService.Create(request);
+            return Ok();
+        }
+    }
+}
