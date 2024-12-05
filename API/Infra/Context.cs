6d63b0936a4a02383328e99ca10e228abb819335
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-12-04 22:08:11 -0300

    creating context

---
+++ API/Infra/Context.cs
@@ -0,0 +1,16 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Infra
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public Context(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
