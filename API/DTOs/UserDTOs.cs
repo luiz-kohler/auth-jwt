673a0221095690344d22d88c178473f7453bf1ed
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-12-05 16:48:40 -0300

    creating user DTOs

---
+++ API/DTOs/UserDTOs.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class UserRequest
+    {
+
+        [Required(ErrorMessage = "empty name", AllowEmptyStrings = false)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "empty email", AllowEmptyStrings = false)]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "invalid email")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "empty password", AllowEmptyStrings = false)]
+        public string Password { get; set; }
+    }
+}
