690aa8da7f812ae3509dd5790656f295104d3f74
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-12-04 22:13:36 -0300

    creating interface of generic repository

---
+++ API/Infra/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Infra
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity> DeleteAsync(TEntity entity);
+        Task<TEntity> FirstOrDefaultAsync(Func<TEntity, bool> predicate);
+        Task<TEntity> GetAllAsync(Func<TEntity, bool>? predicate = null);
+        Task<TEntity> SaveChangesAsync();
+    }
+}
