a9e1f09587d27a05f188d381a542d75e1bf23345
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-12-06 09:45:23 -0300

    creating global exception handler

---
+++ API/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+
+namespace API.Handlers
+{
+    public class GlobalExceptionHandler
+    {
+        public static async Task Handle(HttpContext httpContext)
+        {
+            var exceptionHandlerFeature = httpContext.Features.Get<IExceptionHandlerFeature>();
+            if (exceptionHandlerFeature == null)
+                return;
+
+            var (httpStatusCode, message) = exceptionHandlerFeature.Error switch
+            {
+                BadHttpRequestException exception => (HttpStatusCode.BadRequest, exception.Message),
+                UnauthorizedAccessException exception => (HttpStatusCode.Unauthorized, exception.Message),
+                Exception exception => (HttpStatusCode.InternalServerError, exception.Message + " - " + exception.InnerException),
+                _ => (HttpStatusCode.InternalServerError, "unexpected error")
+            };
+
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)httpStatusCode;
+
+            var jsonResponse = new
+            {
+                httpContext.Response.StatusCode,
+                Message = message,
+            };
+
+            var jsonSerialised = JsonSerializer.Serialize(jsonResponse);
+            await httpContext.Response.WriteAsync(jsonSerialised);
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,16 @@
+using API.Handlers;
 using API.Infra;
 using API.Services;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,6 +20,12 @@ builder.Services.AddDbContext<Context>(opt => opt.UseSqlServer(builder.Configura
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
+builder.Services.AddExceptionHandler(options =>
+{
+    options.ExceptionHandler = GlobalExceptionHandler.Handle;
+    options.AllowStatusCode404Response = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 app.UseHttpsRedirection();
 
